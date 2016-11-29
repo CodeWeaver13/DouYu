0c5ece8d0a3c6d90cbd0067f28b32ad7b72b38eb
高印 <gaoyin_vip@126.com>
2016-11-29 15:20:07 +0800

    提交代码

---
+++ app/src/main/java/com/team/zhouke/MainActivity.java
@@ -0,0 +1,13 @@
+package com.team.zhouke;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
