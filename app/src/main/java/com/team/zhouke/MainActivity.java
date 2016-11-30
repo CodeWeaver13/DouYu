19640b373fc9e4579a33a96f01d6bb9e976cb3fa
高印 <gaoyin_vip@126.com>
2016-11-30 09:57:01 +0800

    修改mainActivity

--- app/src/main/java/com/team/zhouke/MainActivity.java
+++ app/src/main/java/com/team/zhouke/MainActivity.java
@@ -2,7 +2,15 @@ package com.team.zhouke;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+/**
+ *  作者：gaoyin
+ *  电话：18810474975
+ *  邮箱：18810474975@163.com
+ *  版本号：1.0
+ *  类描述：
+ *  备注消息：
+ *  修改时间：2016/11/30 上午9:56
+ **/
 public class MainActivity extends AppCompatActivity {
 
     @Override
