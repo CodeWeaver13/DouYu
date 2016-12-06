2a89de39af89c701acf05a4f8b264259f6925ef2
yolandaLi <liyaling@qkhl.net>
2016-12-06 18:04:47 +0800

    添加SP工具类

--- app/src/main/java/com/team/zhuoke/view/common/activity/SplashActivity.java
+++ app/src/main/java/com/team/zhuoke/view/common/activity/SplashActivity.java
@@ -1,37 +1,36 @@
 package com.team.zhuoke.view.common.activity;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 import com.team.zhuoke.R;
-
-import static android.provider.Telephony.Mms.Part.FILENAME;
+import com.team.zhuoke.utils.SharedPreferenceUtils;
 
 public class SplashActivity extends AppCompatActivity {
     Handler handler;
+    Intent intent;
+    private boolean isFirst;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+//        这步取值操作不能放在run中，否则会执行两次导致页面跳转出错
+        isFirst = SharedPreferenceUtils.getBooleanData("isFirst", true);
+//        LogUtils.e("isFirst=" + isFirst);
         handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+//                LogUtils.e(Thread.currentThread().getName() + "++++");
                 if (isFinishing()) {
                     return;
                 }
-                Intent intent;
-                if (getApplicationContext().getSharedPreferences(FILENAME, Context.MODE_PRIVATE).getBoolean
-                        ("isFirst",true)) {
+                if (isFirst) {
                     intent = new Intent(SplashActivity.this, GuideActivity.class);
-                    SharedPreferences.Editor edit = getApplicationContext().getSharedPreferences(FILENAME, Context.MODE_PRIVATE).edit();
-                    edit.putBoolean("isFirst",false);
-                    edit.commit();
+                    SharedPreferenceUtils.setBooleanData("isFirst", false);
 
                 } else {
                     intent = new Intent(SplashActivity.this, MainActivity.class);
