67893e659f78186a9e70b9790d4b033059df2a66
yolandaLi <liyaling@qkhl.net>
2016-12-06 11:15:58 +0800

    更改APP名称、icon，添加欢迎页

--- app/src/main/java/com/team/zhuoke/view/common/activity/SplashActivity.java
+++ app/src/main/java/com/team/zhuoke/view/common/activity/SplashActivity.java
@@ -18,10 +18,13 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
+                if (isFinishing()) {
+                    return;
+                }
                 Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, 3000);
+        }, 1000);
     }
 }
